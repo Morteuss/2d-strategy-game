c54e8e477c129877cee7dc043f7bf5290031b15b
Morteus <morteuss@protonmail.com>
2023-06-02 17:39:56 +0200

    Small improvements

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main() {
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
-        sf::Event event;
+        sf::Event event{};
         while (window.pollEvent(event))
         {
             // "close requested" event: we close the window
